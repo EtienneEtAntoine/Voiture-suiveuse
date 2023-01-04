20d2a23eec6f1365ef90dc6e9b83700a5388f6c4
EtienneEtAntoine <120325571+EtienneEtAntoine@users.noreply.github.com>
2023-01-04 16:11:59 +0100

    Add files via upload

---
+++ Programme_1.ino
@@ -0,0 +1,11 @@
+const int trig = 3;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(trig,INPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(digitalRead(trig));
+  
+}
